cbdc2c6848f2cd994df3a57df362cbcdc46051b2
MasumaJaffery <masumajaffery1@gmail.com>
2024-01-28 22:51:28 +0500

    introduccion.dart

---
+++ primero/lib/introduccion.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  // print y stdout.write son funciones para imprimir en la consola.
+  print('Hola mundo');
+  stdout.write('Como te llamas?');
+  // stdin.readLineSync() es una funcion para leer de la consola.
+  var nombre = stdin.readLineSync();
+  print('Welcome $nombre');
+}
