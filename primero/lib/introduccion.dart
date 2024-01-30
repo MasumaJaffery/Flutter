2fd1afaf3906445416d5046435d6c14a00785c8b
MasumaJaffery <masumajaffery1@gmail.com>
2024-01-31 03:59:05 +0500

    Variable Scope

--- primero/lib/introduccion.dart
+++ primero/lib/introduccion.dart
@@ -1,12 +1,23 @@
-// ignore_for_file: avoid_print
-
 import 'dart:io';
 
 void main() {
   // print y stdout.write son funciones para imprimir en la consola.
   print('Hola mundo');
-  stdout.write('Como te llamas?');
+  stdout.write('Como te llamas? ');
+
   // stdin.readLineSync() es una funcion para leer de la consola.
   var nombre = stdin.readLineSync();
+
   print('Welcome $nombre');
+
+  // Variables;
+  // nullable or non-nullavle value;
+  int? n;
+  // variable initialization;
+  n = 5;
+  print(n);
+
+  // variable inline declaration;
+  String miNombre = 'MasumaJaffery';
+  print(miNombre);
 }
