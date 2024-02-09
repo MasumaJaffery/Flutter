72be8f40fd671b7236330eb2f43a4e6ff4572b8a
MasumaJaffery <masumajaffery1@gmail.com>
2024-02-10 02:29:31 +0500

    loops

---
+++ primero/lib/loops.dart
@@ -0,0 +1,18 @@
+void main() {
+  for (int i = 0; i <= 10; i++) {
+    print("Saludos a todos!");
+  }
+  int i = 100;
+  do {
+    print("Arcoiris");
+  } while (i < 50);
+
+  int a = 1;
+  while (a <= 12) {
+    print("no is $a");
+    a++;
+  }
+}
+// for loop is used when we know the number of iterations.
+// do-while loop executes at-least one time even condition is true or false.
+// while loop only runs if condition is true.
